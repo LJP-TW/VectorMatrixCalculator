2526b5b1dd1c0a3deb01743ba2f4037745e92e59
LJP-TW <accr94238@gmail.com>
2019-04-01 22:41:44 +0800

    eigen complete eigen vector 跟測資不一樣 不確定是否有影響

--- Project1/Project1/Matrix.cpp
+++ Project1/Project1/Matrix.cpp
@@ -1,5 +1,9 @@
 #include "Matrix.h"
 #include <cstdlib>
+#include <cmath>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 Matrix::Matrix()
 {
@@ -171,52 +175,8 @@ unsigned int Matrix::rank()
 {
 	Matrix A = *this;
 
-	// Gaussian elimination to get r.e.f.
-	for (unsigned int current_y = 0, current_x = 0; \
-		current_y < A.Data.size() && current_x < A.Data[0].size(); \
-		++current_y, ++current_x)
-	{
-		// Search for maximum in this column
-		double maxNum = abs(A.Data[current_y][current_x]);
-		unsigned int maxRow = current_y;
-		for (unsigned int r = current_y + 1; r < A.Data.size(); ++r)
-		{
-			if (abs(A.Data[r][current_x]) > maxNum)
-			{
-				maxNum = abs(A.Data[r][current_x]);
-				maxRow = r;
-			}
-		}
-
-		if (!maxNum)
-		{
-			--current_y;
-			continue;
-		}
-
-		// Swap maxRow to current row
-		for (unsigned int c = current_x; c < A.Data[0].size(); ++c)
-		{
-			double temp = A.Data[current_y][c];
-			A.Data[current_y][c] = A.Data[maxRow][c];
-			A.Data[maxRow][c] = temp;
-		}
-
-		// Doing elimination
-		for (unsigned int r = current_y + 1; r < A.Data.size(); ++r)
-		{
-			if (!A.Data[r][current_x])
-			{
-				continue;
-			}
-
-			for (unsigned int c = current_x + 1; c < A.Data[0].size(); ++c)
-			{
-				A.Data[r][c] -= (A.Data[current_y][c] * A.Data[r][current_x] / A.Data[current_y][current_x]);
-			}
-			A.Data[r][current_x] = 0;
-		}
-	}
+	// Change this matrix into r.e.f.
+	A.ref();
 
 	for (unsigned int y = 0; y < A.Data.size(); ++y)
 	{
@@ -426,7 +386,172 @@ Matrix Matrix::adj()
 
 std::vector<Matrix> Matrix::eigen()
 {
-	return std::vector<Matrix>();
+	if (this->Data.size() != (*this).Data[0].size())
+	{
+		throw MATRIX_ERROR::NON_SQUARE;
+	}
+	else if (this->Data.size() != 2 && this->Data.size() != 3)
+	{
+		throw MATRIX_ERROR::EIGEN_DIMENSION_ERROR;
+	}
+
+	std::vector<double> eigenValues;
+	std::vector<std::vector<double>> eigenVectors;
+	std::vector<Matrix> result;
+	std::vector<double> tempEigenVector;
+	Matrix tempMatrix[2];
+
+	// Find eigenvalues and eigenvectors
+	if (this->Data.size() == 2)
+	{
+		// | A00 - D      A01 |
+		// | A10      A11 - D |
+		// D^2 + bD + c = 0
+		// a = 1
+		// b = (-A11) + (-A00)
+		// c = A00 * A11 - A01 * A10
+		double b, c;
+		b = -this->Data[0][0] - this->Data[1][1];
+		c = this->Data[0][0] * this->Data[1][1] - this->Data[0][1] * this->Data[1][0];
+
+		// Get eigenvalues by Quadric Equation
+		eigenValues.push_back((-b + sqrt(b * b - 4 * c)) / 2);
+		eigenValues.push_back((-b - sqrt(b * b - 4 * c)) / 2);
+
+		// Get eigenvectors
+		tempEigenVector.push_back(this->Data[0][1]);
+		tempEigenVector.push_back(eigenValues[0] - this->Data[0][0]);
+		eigenVectors.push_back(tempEigenVector);
+
+		tempEigenVector.clear();
+		tempEigenVector.push_back(this->Data[1][1] - eigenValues[1]);
+		tempEigenVector.push_back(this->Data[1][0]);
+		eigenVectors.push_back(tempEigenVector);
+	}
+	else if (this->Data.size() == 3)
+	{
+		// | A00 - D      A01      A02 |
+		// | A10      A11 - D      A12 |
+		// | A20          A21  A22 - D | 
+		// aD^3 + bD^2 + cD + d = 0
+		// a = -1
+		// b = A00 + A11 + A22
+		// c = - A00 * A11 
+		//     - A11 * A22 
+		//     - A22 * A00
+		//	   + A01 * A10
+		//     + A02 * A20
+		//	   + A12 * A21
+		// d = + A00 * A11 * A22 
+		//     + A01 * A12 * A20 
+		//     + A02 * A10 * A21 
+		//     - A02 * A11 * A20 
+		//     - A00 * A12 * A21 
+		//     - A01 * A10 * A22
+		// If a = 1, bcd will *= -1
+		double b, c, d, q, r, t;
+		b = -this->Data[0][0] - this->Data[1][1] - this->Data[2][2];
+
+		c = + this->Data[0][0] * this->Data[1][1] \
+			+ this->Data[1][1] * this->Data[2][2] \
+			+ this->Data[2][2] * this->Data[0][0] \
+			- this->Data[0][1] * this->Data[1][0] \
+			- this->Data[0][2] * this->Data[2][0] \
+			- this->Data[1][2] * this->Data[2][1];
+
+		d = - this->Data[0][0] * this->Data[1][1] * this->Data[2][2] \
+			- this->Data[0][1] * this->Data[1][2] * this->Data[2][0] \
+			- this->Data[0][2] * this->Data[1][0] * this->Data[2][1] \
+			+ this->Data[0][2] * this->Data[1][1] * this->Data[2][0] \
+			+ this->Data[0][0] * this->Data[1][2] * this->Data[2][1] \
+			+ this->Data[0][1] * this->Data[1][0] * this->Data[2][2];
+
+		q = (b * b - 3 * c) / 9;
+		r = (2 * b * b * b - 9 * b * c + 27 * d) / 54;
+		t = acos(r / (sqrt(q * q * q)));
+
+		// Get eigenvalues by Cubic Equation
+		eigenValues.push_back(-2 * sqrt(q) * cos(t / 3) - b / 3);
+		eigenValues.push_back(-2 * sqrt(q) * cos((t + 2 * M_PI) / 3) - b / 3);
+		eigenValues.push_back(-2 * sqrt(q) * cos((t - 2 * M_PI) / 3) - b / 3);
+		
+		// Get eigenvectors
+		for (unsigned int n = 0; n < 3; ++n)
+		{
+			// Produce (A - DnI)
+			// Dn: nth eigenvalue
+			// I : Identity matrix
+			Matrix A = *this;
+			for (unsigned int r = 0; r < 3; ++r)
+			{
+				A.Data[r][r] -= eigenValues[n];
+			}
+
+			A.ref();
+
+			if (A.Data[1][1] == 0)
+			{
+				// (A - DnI) = 
+				// | a00 a01 a02 |
+				// |   0   0 a12 |
+				// |   0   0   0 |
+				// x0 = a01
+				// x1 = -a00
+				// x2 = 0
+				tempEigenVector.push_back(A.Data[0][1]);
+				tempEigenVector.push_back(-A.Data[0][0]);
+				tempEigenVector.push_back(0);
+				eigenVectors.push_back(tempEigenVector);
+			}
+			else
+			{
+				// (A - DnI) = 
+				// | a00 a01 a02 |
+				// |   0 a11 a12 |
+				// |   0   0   0 |
+				// x0 = (a11 * a02 - a12 * a01) / a00
+				// x1 = a12
+				// x2 = -a11
+				tempEigenVector.push_back((A.Data[1][1] * A.Data[0][2] - A.Data[1][2] * A.Data[0][1]) / A.Data[0][0]);
+				tempEigenVector.push_back(A.Data[1][2]);
+				tempEigenVector.push_back(-A.Data[1][1]);
+				eigenVectors.push_back(tempEigenVector);
+			}
+			tempEigenVector.clear();
+		}
+
+	}
+
+	// Produce matrix containing eigenvectors as columns 
+	for (unsigned int i = 0; i < eigenVectors.size(); ++i)
+	{
+		tempMatrix[0].Data.push_back(eigenVectors[i]);
+	}
+	tempMatrix[0].trans();
+
+	// Produce diagonal eigenvalues matrix
+	for (unsigned int i = 0; i < eigenValues.size(); ++i)
+	{
+		std::vector<double> tempRow;
+		for (unsigned int c = 0; c < eigenValues.size(); ++c)
+		{
+			if (c == i)
+			{
+				tempRow.push_back(eigenValues[i]);
+			}
+			else
+			{
+				tempRow.push_back(0);
+			}
+		}
+		tempMatrix[1].Data.push_back(tempRow);
+	}
+
+	// Produce result
+	result.push_back(tempMatrix[0]);
+	result.push_back(tempMatrix[1]);
+	
+	return result;
 }
 
 std::vector<Matrix> Matrix::pm()
@@ -437,4 +562,54 @@ std::vector<Matrix> Matrix::pm()
 Matrix leastsquare(const Matrix & A, const Matrix & B)
 {
 	return Matrix();
+}
+
+void Matrix::ref()
+{
+	// Gaussian elimination to get r.e.f.
+	for (unsigned int current_y = 0, current_x = 0; \
+		current_y < this->Data.size() && current_x < this->Data[0].size(); \
+		++current_y, ++current_x)
+	{
+		// Search for maximum in this column
+		double maxNum = abs(this->Data[current_y][current_x]);
+		unsigned int maxRow = current_y;
+		for (unsigned int r = current_y + 1; r < this->Data.size(); ++r)
+		{
+			if (abs(this->Data[r][current_x]) > maxNum)
+			{
+				maxNum = abs(this->Data[r][current_x]);
+				maxRow = r;
+			}
+		}
+
+		if (!maxNum)
+		{
+			--current_y;
+			continue;
+		}
+
+		// Swap maxRow to current row
+		for (unsigned int c = current_x; c < this->Data[0].size(); ++c)
+		{
+			double temp = this->Data[current_y][c];
+			this->Data[current_y][c] = this->Data[maxRow][c];
+			this->Data[maxRow][c] = temp;
+		}
+
+		// Doing elimination
+		for (unsigned int r = current_y + 1; r < this->Data.size(); ++r)
+		{
+			if (!this->Data[r][current_x])
+			{
+				continue;
+			}
+
+			for (unsigned int c = current_x + 1; c < this->Data[0].size(); ++c)
+			{
+				this->Data[r][c] -= (this->Data[current_y][c] * this->Data[r][current_x] / this->Data[current_y][current_x]);
+			}
+			this->Data[r][current_x] = 0;
+		}
+	}
 }
